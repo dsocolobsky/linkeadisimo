3e329901a0614a5dd8c9f71f883659c1f19db3d8
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-12-24 20:15:09 -0300

    fixes path for pub

--- project/website/urls.py
+++ project/website/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pub/<int:pub_id>/', views.publication, name="publication"),
+    path('user/<int:user_id>/', views.user, name="user"),
 ]
--- project/website/views.py
+++ project/website/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import User, Publication
 
 # Create your views here.
 def index(request):
-    return HttpResponse('This is the index')
+    publications = Publication.objects.order_by('-date')
+    context = {
+        'publications': publications
+    }
+    return render(request, 'website/index.html', context)
+
+def publication(request, pub_id):
+    pub = get_object_or_404(Publication, pk=pub_id)
+    context = {
+        'publication': pub
+    }
+    return render(request, 'website/publication.html', context)
+
+def user(request, user_id):
+    return HttpResponse('user')
