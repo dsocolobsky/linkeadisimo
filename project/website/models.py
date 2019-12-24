4882b40fb68b2476d60e48bbe54dab6ca28873c5
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-12-24 16:15:52 -0300

    text and link can be empty in pub

--- project/website/models.py
+++ project/website/models.py
@@ -10,9 +10,9 @@ class User(models.Model):
 
 class Publication(models.Model):
     is_text = models.BooleanField()
-    link = models.URLField()
-    text = models.TextField()
-    votes = models.IntegerField()
+    link = models.URLField(blank=True)
+    text = models.TextField(blank=True)
+    votes = models.IntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.SET(None))
 
