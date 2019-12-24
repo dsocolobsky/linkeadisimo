6e9e10ade2a3692d879f407e26df677d9ac8d582
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-12-24 16:04:39 -0300

    make initial migrations

--- project/website/models.py
+++ project/website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=128)
+
+class Publication(models.Model):
+    is_text = models.BooleanField()
+    link = models.URLField()
+    text = models.TextField()
+    votes = models.IntegerField()
+    date = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(User, on_delete=models.SET(None))
