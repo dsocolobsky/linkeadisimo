fcd7ff9c4f32da3af971b89ef157421f52467462
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-12-26 19:54:56 -0300

    switched to class-based Views for several routes

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pub/<int:pub_id>/', views.publication, name="publication"),
     path('user/<int:user_id>/', views.user, name="user"),
-    path('submit', views.submit, name="submit"),
+    path('submit', views.Submit.as_view(), name="submit"),
     path('logout_view', views.logout_view, name="logout_view"),
-    path('login', views.login, name="login"),
+    path('login', views.Login.as_view(), name="login"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadReque
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import View
 from django.contrib.auth import logout
 from django.contrib.auth.models import Permission, User
 from .models import Publication
@@ -33,37 +34,34 @@ def publication(request, pub_id):
 def user(request, user_id):
     return HttpResponse('user')
 
-def submit(request):
-    if request.method == 'POST':
-        print("it's a post")
-        return submitPOST(request)
-    else:
+class Submit(View):
+    def get(self, request):
         return render(request, 'website/submit.html')
 
-def submitPOST(request):
-    title = request.POST['title']
-    text = request.POST['text']
-    url  = request.POST['url']
-    user = User.objects.get(pk=1)
+    def post(self, request):
+        title = request.POST['title']
+        text = request.POST['text']
+        url  = request.POST['url']
+        user = User.objects.get(pk=1)
 
-    if text != '' and url != '':
-        return HttpResponseBadRequest('<p>You must only submit EITHER url OR text</p>')
+        if text != '' and url != '':
+            return HttpResponseBadRequest('<p>You must only submit EITHER url OR text</p>')
 
-    if text == '':
-        print("It's text")
-        # We have to submit an URL
-        pub = Publication(is_text=False, title=title, link=url, created_by=user)
-    else:
-        print("It's url")
-        # We have to submit the text
-        pub = Publication(is_text=True, title=title, text=text, created_by=user)
+        if text == '':
+            pub = Publication(is_text=False, title=title, link=url, created_by=user)
+        else:
+            pub = Publication(is_text=True, title=title, text=text, created_by=user)
 
-    pub.save()
-    return HttpResponseRedirect(reverse('publication', args=(pub.id,)))
+        pub.save()
+        return HttpResponseRedirect(reverse('publication', args=(pub.id,)))
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
 
-def login(request):
-    return HttpResponseNotFound()
+class Login(View):
+    def get(self, request):
+        return render(request, 'website/login.html')
+
+    def post(self, request):
+        return HttpResponseBadRequest('<p>Not yet implemented</p>')
