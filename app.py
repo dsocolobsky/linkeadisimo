d56e8111daf9310411496b93cd7ba89f2a1df7d5
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-12-27 00:29:42 -0300

    delete files from old flask version

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['DEBUG'] = True
-
--- database.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from flask_sqlalchemy import SQLAlchemy
-from app import app
-
-db = SQLAlchemy(app)
