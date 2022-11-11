7860bcb2613765960e1c1124cae517a6282447b9
Dylan Socolobsky <dsocolobsky@gmail.com>
2022-11-11 18:31:57 -0300

    cleanup: remove upvote related code from js file

--- website/static/website/js/linkeadisimo.js
+++ website/static/website/js/linkeadisimo.js
@@ -1,11 +1,8 @@
-console.log('ohaio');
+console.log('linkeadisimo.js initializating');
 
 let prevShownForm = null;
 function init_comment(com) {
-    console.log("Initing comment " + com.id);
-
     let answer_button = com.querySelector('.answer_comment');
-    let uparrow = com.querySelector('.uparrow');
     let form = com.querySelector('.comment_form');
 
     if (answer_button != null) {
@@ -16,48 +13,6 @@ function init_comment(com) {
             prevShownForm = form;
         });
     }
-
-    if (uparrow != null) {
-        uparrow.addEventListener('click', () => {
-            console.log("Voting for " + com.id);
-            axios({
-                method: 'post',
-                url: 'http://localhost:8000/upvote_comment',
-                headers: {
-                    "X-CSRFToken": csrftoken,
-                },
-                data: {
-                    id: com.id,
-                }
-            }).then(data => {
-                console.log("Voted " + com.id);
-                location.reload()
-            })
-                .catch(err => console.log(err))
-        });
-    }
-}
-
-function init_publications(pub) {
-    let uparrow = pub.querySelector('.uparrow');
-
-    uparrow.addEventListener('click', () => {
-        console.log('Clicked ' + pub.id);
-
-        axios({
-            method: 'post',
-            url: 'http://localhost:8000/upvote',
-            headers: {
-                "X-CSRFToken": csrftoken,
-            },
-            data: {
-                id: pub.id,
-            }
-        }).then(data => location.reload())
-            .catch(err => console.log(err))
-    });
 }
 
 document.querySelectorAll('.comment').forEach(init_comment);
-
-document.querySelectorAll('.publication').forEach(init_publications);
