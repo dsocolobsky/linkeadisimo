4391bc710a0ca17f2f251525f98d385380bee85b
Dylan Socolobsky <dsocolobsky@gmail.com>
2023-01-17 16:56:47 -0300

    chore: prepare for prod

--- linkeadisimo/settings.py
+++ linkeadisimo/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "SECRET_CHANGE_THIS_LATER"
+SECRET_KEY = os.getenv("LINKEADISIMO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.1.88"]
 
